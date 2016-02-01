bf2481290d8d00e57e04a9f8c54b343febfb682f
darwinsenior <darwinsenior@gmail.com>
2016-02-01 14:40:43 -0600

    finish the first question

--- assignment1/first.R
+++ assignment1/first.R
@@ -1,58 +1,60 @@
-system("wget http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data")
+# system("wget http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data")
 #reading data
-d <- read.csv('./pima-indians-diabetes.data', sep=",")
-d <- data.matrix(d)
+library(devtools)
+source_url("https://raw.githubusercontent.com/ggrothendieck/gsubfn/master/R/list.R")
+d <- read.csv('./pima-indians-diabetes.data', sep=",", header=T)
+dx <- d[, -c(9)]
+dy <- d[, 9]
 
 library('caret')
 # preparing data with 80% traing and 20% testing
-inTrain <- createDataPartition(d[,1], p=0.8, list=F)
+inTrain <- createDataPartition(dy, p=0.8, list=F)
 
 trainData = d[inTrain,]
 testData = d[-inTrain,]
 
-trainX <- trainData[,-9]
+trainX <- trainData[,-c(9)]
 trainY <- trainData[,9]
-testX <- testData[,-9]
+testX <- testData[,-c(9)]
 testY <- testData[,9]
 
-posFilter <- testY==1
 # using gaussian distribution so that we know the best
-trainMuP <- apply(trainX[posFilter,], 2, mean)
-trainMuN <- apply(trainX[!posFilter,], 2, mean)
-trainSigmaP <- apply(trainX[posFilter,], 2, sd)
-trainSigmaN <- apply(trainX[!posFilter,], 2, sd)
 
-# first problem
-pos <- array(0,length(testY))
-neg <- array(0,length(testY))
+trainGaussian <- function(trainX, trainY){
+    posFilter <- trainY == 1
+    trainMuP <- sapply(trainX[posFilter,], mean, na.rm=T)
+    trainMuN <- sapply(trainX[!posFilter,], mean, na.rm=T)
+    trainSigmaP <- sapply(trainX[posFilter,], sd, na.rm=T)
+    trainSigmaN <- sapply(trainX[!posFilter,], sd, na.rm=T)
+    return(list(trainMuP, trainMuN, trainSigmaP, trainSigmaN))
+}
 
-for (i in 1:8)
-    pos = pos+dnorm(testX[,i], mean=trainMuP[i], sd=trainSigmaP[i], log=T)
-    neg = neg+dnorm(testX[,i], mean=trainMuN[i], sd=trainSigmaN[i], log=T)
-end
+perdictGaussian <- function(x, muP, muN, sigmaP, sigmaN){
+    offsetP <- t(t(x)-muP) #x-mu
+    offsetN <- t(t(x)-muN)
+    scaleP <- t(t(offsetP) / sigmaP) # (x-mu)/sigma
+    scaleN <- t(t(offsetN) / sigmaN)
+    logP <- -0.5*rowSums(apply(scaleP, c(1,2), square), na.rm=T)-sum(log(sigmaP))
+    # (x-mu)^2/2sigma^2-log(simga)
+    logN <- -0.5*rowSums(apply(scaleN, c(1,2), square), na.rm=T)-sum(log(sigmaN))
+    return(logP>logN)
+}
 
-predictY <- array(0, length(testY))
-predictY[pos>neg] <- 1
+correctPercent <- function(x, y) sum(x==y)/length(x)
 
-for (i in c(3,6,4,8))
-    trainX[trainX[,i]==0,i] = NA
-end
+correctness = array(dim=10)
 
-meanna <- function(x){mean(x,na.rm=T)}
-sdna <- function(x){sd(x,na.rm=T)}
-# second problem
-trainMuP <- apply(trainX[posFilter,], 2, meanna)
-trainMuN <- apply(trainX[!posFilter,], 2, meanna)
-trainSigmaP <- apply(trainX[posFilter,], 2, sdna)
-trainSigmaN <- apply(trainX[!posFilter,], 2, sdna)
+for (wi in 1:10){
+    inTrain <- createDataPartition(dy, p=0.8, list=F)
 
-pos <- array(0, length(testY))
-neg <- array(0, length(testY))
+    trainData = d[inTrain,]
+    testData = d[-inTrain,]
+    trainX = trainData[,-c(9)]
+    trainY = trainData[,9]
+    testX = testData[,-c(9)]
+    testY = testData[,9]
 
-for (i in 1:8)
-    pos = pos+dnorm(testX[,i], mean=trainMuP[i], sd=trainSigmaP[i], log=T)
-    neg = neg+dnorm(testX[,i], mean=trainMuN[i], sd=trainSigmaN[i], log=T)
-end
+    list[meanP, meanN, sdP, sdN] = trainGaussian(trainX, trainY)
+    correctness = correctPercent(perdictGaussian(testX, meanP, meanN, sdP, sdN), testY)
+}
 
-
-model = train(d[,-9], d[,9]==1, 'nb', metric="Accuracy")
